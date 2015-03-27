8deeb29668cc387bfe53d56ee976a8354c489379
Steve Cobb <scobb@alumni.lsu.edu>
2015-03-26 21:52:25 -0500

    Adding readme.

---
+++ src/README.sh
@@ -0,0 +1,17 @@
+Steve Cobb and Nate Zuiker
+360P assignment 4
+
+Compile instructions:
+Compile with java 8.
+Run build.sh.
+
+Optionally, manually compile each java file.
+
+Run instructions:
+SERVER
+$ java server/Server
+
+CLIENT
+$ java client/Client
+
+
